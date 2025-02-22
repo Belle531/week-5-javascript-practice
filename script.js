90a2eb313e25d2cf0e2cdc5facbc24817ce8ef2c
Cassandra <cassandra.moore1516@gmail.com>
2025-02-21 21:17:45 -0600

    lost all work added it all back last file would not allow me to commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let userScore = 150;
+let name = "Alice";
+document.write(`Hi ${name}, your score is ${userScore}<br>`);
+
+let timeOfDay = "morning";
+let user = "Bob";
+document.write(`Good ${timeOfDay}, ${user}<br>`);
+
+let price = 10;
+let quantity = 3;
+let total = price * quantity;
+document.write(`The total is ${total}<br>`);
+
+let animal = "cat";
+let action = "slept";
+document.write(`${animal} ${action} all day.<br>`);
+
+let itemName = "Laptop "; // Trailing space here (be mindful of it)
+let itemPrice = 999;
+document.write(`The ${itemName} costs $${itemPrice}.<br>`);
+
